99f641c65ab4a7cda8a06e5588d73415c157d87f
chongyaoo <e0673901@u.nus.edu>
2025-08-24 23:53:09 +0800

    Update Grizzly.java

---
+++ src/main/java/Grizzly.java
@@ -0,0 +1,10 @@
+public class Grizzly {
+    public static void main(String[] args) {
+        String logo = """
+                Hi, I'm your friendly assistant, Grizzly!
+                What can I do for you today?
+                ____________________________________________________________
+                Bye. Hope to see you again soon!""";
+        System.out.println(logo);
+    }
+}
