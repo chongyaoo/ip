6f4fe42c6e558ef48ff6bc788937e9ca70412e48
chongyaoo <e0673901@u.nus.edu>
2025-09-02 16:47:28 +0800

    Update Grizzly.java
    Implemented some changes to the logic of chatbot

--- src/main/java/Grizzly.java
+++ src/main/java/Grizzly.java
@@ -1,3 +1,4 @@
+
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -5,7 +6,7 @@ public class Grizzly {
     public static void main(String[] args) {
         String line;
         String[] storedItems = new String[100];
-        int index = 0;
+        int index = 1;
         int itemToMark = 0;
         int itemToUnmark = 0;
         boolean[] marked = new boolean[100];
@@ -17,34 +18,45 @@ public class Grizzly {
         System.out.println(logo);
         line = in.nextLine();
         while (!Objects.equals(line, "bye")) {
-            if (!Objects.equals(line, "list") && !line.contains("mark") && !line.contains("unmarked")) {
-                System.out.println("added: " + line);
-                storedItems[index] = line;
-                index++;
-                line = in.nextLine();
-            } else if (line.contains("unmark")) {
-                String[] items = line.split(" ");
-                itemToMark = Integer.parseInt(items[1]);
-                marked[itemToUnmark] = false;
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("[ ] " + storedItems[itemToUnmark]);
-                line = in.nextLine();
-            } else if (line.contains("mark")) {
-                String[] items = line.split(" ");
-                itemToMark = Integer.parseInt(items[1]);
-                marked[itemToMark] = true;
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[X] " + storedItems[itemToMark]);
-                line = in.nextLine();
-            } else {
+            if (Objects.equals(line, "list")) {
                 System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < index; i++) {
+                for (int i = 1; i < index; i++) {
                     if (marked[i]) {
                         System.out.println(i + ".[" + "X" + "] " + storedItems[i]);
                     } else {
                         System.out.println(i + ".[" + " " + "] " + storedItems[i]);
                     }
                 }
+                line = in.nextLine();
+            } else if (line.contains("unmark")) {
+                String[] items = line.split(" ");
+                itemToUnmark = Integer.parseInt(items[1]);
+                if (itemToUnmark < index) {
+                    marked[itemToUnmark] = false;
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("[ ] " + storedItems[itemToUnmark]);
+                    line = in.nextLine();
+                }
+                else {
+                    System.out.println("Task " + itemToUnmark + " has not been created!");
+                    line = in.nextLine();
+                }
+            } else if (line.contains("mark")) {
+                String[] items = line.split(" ");
+                itemToMark = Integer.parseInt(items[1]);
+                if (itemToMark < index) {
+                    marked[itemToMark] = true;
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[X] " + storedItems[itemToMark]);
+                    line = in.nextLine();
+                } else {
+                    System.out.println("Task " + itemToMark + " has not been created!");
+                    line = in.nextLine();
+                }
+            } else {
+                System.out.println("added: " + line);
+                storedItems[index] = line;
+                index++;
                 line = in.nextLine();
             }
         }
